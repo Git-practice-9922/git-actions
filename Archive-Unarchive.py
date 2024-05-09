e95f46b1926f5dc45e70f31d5d24f14be53e0d95
KrushnaPimpale <kpimple656@gmail.com>
2024-05-09 10:06:52 +0530

    simplified

--- Archive-Unarchive.py
+++ Archive-Unarchive.py
@@ -5,7 +5,7 @@ import requests
 def archive_repo(repo_name):
     response = requests.patch(
         f"https://api.github.com/repos/Git-practice-9922/{repo_name}",
-        headers={"Authorization": f"Bearer {os.getenv('GITHUB_TOKEN')}"},
+        headers={"Authorization": f"Bearer {os.getenv(GITHUB_TOKEN)}"},
         json={"archived": True}
     )
     return response
